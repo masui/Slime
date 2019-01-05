4a0227e90b328a2d4d1b186e22298d65804161f7
Toshiyuki Masui <masui@pitecan.com>
2019-01-05 17:45:01 +0900

    Google関連を使わないことにすると動くようだ

--- app/src/main/java/com/pitecan/slime/Slime.java
+++ app/src/main/java/com/pitecan/slime/Slime.java
@@ -1,68 +1,233 @@
+//
+//	Slime for Android
+//
+//	Toshiyuki Masui 2010/9/22 (Lexierra.Android)
+//      Slimeとして頑張る 2011/11/20
+//
+
 package com.pitecan.slime;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.inputmethodservice.InputMethodService;
+import android.view.inputmethod.CompletionInfo;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.view.inputmethod.InputMethodManager;
 
+import android.os.Bundle;
+import android.os.Build; // for Build.VERSION.SDK_INT
+import android.view.View;
+import android.view.KeyEvent;
+import android.widget.Button;
 import android.util.Log;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.widget.AbsoluteLayout; // これがあるとコンパイラに文句を言われる
 
-public class Slime extends AppCompatActivity {
+import android.content.Context;
+import android.text.ClipboardManager;
+// Android3.0以上の場合こちら
+// import android.content.ClipData;
+// import android.content.ClipboardManager;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import android.content.res.AssetManager;
+
+public class Slime extends InputMethodService 
+{
+    private Keys keys;
+    private KeyView keyView;
+    private KeyController keyController;
+    private LocalDict dict;
+    private SQLDict sqlDict;
+    private Search search;
+    
+    private ClipboardManager cm;
+    private String clipboardText;
+    
+    static final int MAXCANDS = 50;
+    
+    /**
+     * Main initialization of the input method component.  Be sure to call
+     * to super class.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_slime);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onCreate()
+    {
+        super.onCreate();
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        AssetManager as;
+        as = getResources().getAssets();
+        Log.v("Slime","as");
+        
+        //dict = new LocalDict(getResources().getAssets());
+        dict = new LocalDict(as);
+        sqlDict = new SQLDict(this);
+        search = new Search(dict,sqlDict,this);
+        cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        CharSequence seq = cm.getText();
+        clipboardText = (seq == null ? "" : seq.toString());
+        
+        //
+        // IMEでBackボタンを扱えるようにするためのもの。
+        // android-11 以降のAPIじゃないと動かない模様
+        //
+        // Log.v("Slime","version="+Build.VERSION.SDK_INT);
+        if(Build.VERSION.SDK_INT >= 11){
+            setBackDisposition(InputMethodService.BACK_DISPOSITION_WILL_NOT_DISMISS);
+        }
+        
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+    }
+    
+    /**
+     * This is the point where you can do all of your UI initialization.  It
+     * is called after creation and any configuration change.
+     */
+    @Override public void onInitializeInterface() {
+        super.onInitializeInterface(); // 必要??
+        keys = new Keys();
+        keyController = new KeyController();
+    }
+    
+    /**
+     * This is the main point where we do our initialization of the input method
+     * to begin operating on an application.  At this point we have been
+     * bound to the client, and are now receiving all of the detailed information
+     * about the target of our edits.
+     */
+    @Override public void onStartInput(EditorInfo attribute, boolean restarting) {
+        super.onStartInput(attribute, restarting);
+    }
+    
+    /**
+     * Called by the framework when your view for creating input needs to
+     * be generated.  This will be called the first time your input method
+     * is displayed, and every time it needs to be re-created such as due to
+     * a configuration change.
+     */
+    @Override public View onCreateInputView() {
+        super.onCreateInputView(); // 必要??
+        /*
+          KeyControllerクラスから他にアクセスできるようにセットする
+        */
+        keyView = (KeyView) getLayoutInflater().inflate(R.layout.input, null);
+        keyView.keys = keys;
+        keyView.keyController = keyController;
+        keyController.search = search;
+        keyController.keyView = keyView;
+        keyController.keys = keys;
+        keyController.dict = dict;
+        keyController.sqlDict = sqlDict;
+        keyController.slime = this;
+        keyController.reset();
+        return keyView;
+    }
+    
+    @Override public void onStartInputView(EditorInfo info, boolean restarting) {
+        super.onStartInputView(info,restarting); // 必要??
+        //
+        // 別アプリに切り替わったときなど初期化する
+        // InputConnection ic = getCurrentInputConnection();
+        // if(ic != null) ic.commitText("",1);
+        // !!!! このコードがあるせいでブラウザのURL枠をタップしたときURLが消えてしまっていた
+        // 何故こういうコードを書いたのか全く覚えていない。 (2012/12/13 21:40:11)
+        //
+        
+        onFinishInput();
+        keyController.resetInput();
+    }
+    
+    @Override public void onFinishInput() {
+        super.onFinishInput();
+    }
+    
+    public void input(String s){
+        InputConnection ic = getCurrentInputConnection();
+        if(ic != null) ic.commitText(s,1); // 入力貼り付け
+    }
+    
+    public void keyDownUp(int keyEventCode) { // キー入力
+        InputConnection ic = getCurrentInputConnection();
+        if(ic != null){
+            ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, keyEventCode));
+            ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_UP, keyEventCode));
+        }
+    }
+
+    public void showComposingText(){
+        String composingText = "";
+        for(int i=0;i<keyController.inputPatArray.size();i++){
+            //Log.v("Slime","pat=" + keyController.inputPatArray.get(i));
+            // 曖昧キーの場合 "[" と "]" で囲む
+            if(keyController.inputPatArray.get(i).matches(".*\\[..*")){
+                composingText += "[" + keyController.inputCharArray.get(i) + "]";
             }
-        });
-
-        Log.v("Slime","----------------------------------------------------");
-
-        Resources res = getResources();
-        String string = res.getString(R.string.test_string);
-        Log.v("Slime",string);
-
-        Log.v("Slime",String.valueOf(R.drawable.ic_launcher_background));
-        Bitmap b;
-        b = BitmapFactory.decodeResource(res,R.drawable.ic_launcher_background);
+            else {
+                composingText += keyController.inputCharArray.get(i);
+            }
+        }
+        InputConnection ic = getCurrentInputConnection();
+        if(ic != null){
+            ic.setComposingText(composingText,1);
+        }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_slime, menu);
-        return true;
+    
+    public void hide(){
+        requestHideSelf(0); // IMEのViewを隠す
     }
-
+    
+    //
+    // 新規登録用にクリップボードの単語を返す
+    //
+    public void clearRegWord(){
+        CharSequence seq = cm.getText();
+        clipboardText = (seq == null ? "" : seq.toString());
+    }
+    
+    public String getRegWord(){
+        CharSequence seq = cm.getText();
+        String s = (seq == null ? "" : seq.toString());
+        if(s.equals(clipboardText)){
+            return "";
+        }
+        else {
+            return s;
+        }
+    }
+    
+    //
+    // キーボードが表示されているときBackボタンが押されたら対応する
+    //
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //Log.v("Slime","onKeyDown - keyCode = "+keyCode);
+        if(Build.VERSION.SDK_INT >= 11 && keyCode == KeyEvent.KEYCODE_BACK && isInputViewShown()){
+            keyController.backKey();
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
+        else {
+            return super.onKeyDown(keyCode,event);
+        }
+    }
+    
+    //
+    // ConnectivityManagerというのを使って、ネットが使えるかどうかを判断し、
+    // ネットがあるときは常にGoogleIMEを使うようにしてみる
+    // http://yife.hateblo.jp/entry/2012/10/29/203330
+    // http://wada811.blog.fc2.com/?tag=ConnectivityManager
+    // AndroidManifest.xmlに以下の追加が必要である
+    // <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+    //
+    // メソッドがActivityの中でしか使えないのでここで定義する。
+    //
+    public Boolean isConnected(){
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
